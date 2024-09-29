1d1b7c36880ce9645b18cd8efd297f04dc3b394b
raylinpeguero78 <120321236+raylinpeguero78@users.noreply.github.com>
2024-09-28 21:06:27 -0400

    Add files via upload
    Esta página web es una solución sencilla pero efectiva para una ferretería que quiere ofrecer sus productos en línea. Su diseño responsivo asegura una excelente experiencia de usuario en cualquier dispositivo, y la integración de un carrito de compras mejora la interactividad del sitio.

---
+++ codigo.js
@@ -0,0 +1,18 @@
+
+let toglee = document.querySelector('.fa-solid');
+let menu = document.querySelector(' .nav__ul')
+
+toglee.addEventListener('click', ()=>{
+menu.classList.toggle('active');
+
+ 
+  
+ 
+
+  //visibility: hidden visible;
+
+
+  console.log("mmg");
+
+
+})
